71d9c3b2132ea8ca9d064a00952a755eb791a803
Boom3kUbuntuMateBookXPro <boomboom3k@gmail.com>
2019-07-22 09:23:37 -0400

    Now uses scopes file and works perfectly!

--- src/main/java/com/boom3k/googletokengenerator/Main.java
+++ src/main/java/com/boom3k/googletokengenerator/Main.java
@@ -5,8 +5,7 @@ import net.lingala.zip4j.exception.ZipException;
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) throws IOException, ZipException {
-        TokenGenerator.createConfigurationFile("google_configuration.json");
+        TokenGenerator.createConfigurationFile("tokengenerator");
     }
 }
--- src/main/java/com/boom3k/googletokengenerator/TokenGenerator.java
+++ src/main/java/com/boom3k/googletokengenerator/TokenGenerator.java
@@ -16,6 +16,8 @@ import net.lingala.zip4j.exception.ZipException;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -26,10 +28,41 @@ public class TokenGenerator {
     private static final JacksonFactory JSON_FACTORY = new JacksonFactory();
 
     static public void createConfigurationFile(String configurationName) throws IOException, ZipException {
+        System.out.println("************* Google Token Generator Begin *************");
+
+        /**--------------Do Scopes stuff--------------*/
+        //Set admin scopes
+        final String[] adminSDKScopes = new String[]{
+                "https://www.googleapis.com/auth/admin.reports.audit.readonly",
+                "https://www.googleapis.com/auth/admin.reports.usage.readonly",
+                "https://www.googleapis.com/auth/admin.directory.user",
+                "https://www.googleapis.com/auth/admin.directory.group",
+                "https://www.googleapis.com/auth/admin.directory.group.member",
+                "https://www.googleapis.com/auth/admin.directory.customer",
+                "https://www.googleapis.com/auth/admin.directory.domain",
+                "https://www.googleapis.com/auth/apps.groups.settings"
+        };
+
+        //Set serviceaccount scopes
+        ArrayList<String> allScopes = new ArrayList<>();
+
+        Scanner scopesScanner = new Scanner(new File("scopes.txt"));
+        while (scopesScanner.hasNextLine()) {
+            String currentLine = scopesScanner.nextLine();
+            if (!currentLine.isEmpty() || !currentLine.isBlank()) {
+                allScopes.add(scopesScanner.nextLine().replace(",", ""));
+            }
+        }
+
+        allScopes.addAll(Arrays.asList(adminSDKScopes));
+        System.out.println("Project Scopes: ");
+
+        allScopes.forEach(scope -> System.out.println(scope));
+        final ImmutableSet<String> SCOPES_SET = ImmutableSet.copyOf(allScopes);
+
         System.out.println("Enter your admin account email:");
         String adminEmail = new BufferedReader(new InputStreamReader(System.in)).readLine();
         String domain = adminEmail.substring(adminEmail.lastIndexOf("@") + 1);
-
 
         /**--------------Credentials Zip File--------------*/
         //GetZip
@@ -41,68 +74,49 @@ public class TokenGenerator {
 
         //GetZipPassword
         System.out.println("Enter Oauth2 credentials zip file password:");
-        String credentialsPassword = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        Console console = System.console();
+        String credentialsPassword =
+                //console.readPassword("Enter Oauth2 credentials zip file password: ").toString();
+                new BufferedReader(new InputStreamReader(System.in)).readLine().replace("\n", "");
+
 
         //Get files file from zip
+        System.out.println("ZipFile: ," + zipPath + " > Attempting to unlock..");
         Map<String, InputStream> zipFiles = Zip3k.getAllInputStreamsInSize(zipPath, credentialsPassword);
+        System.out.println("ZipFile: ," + zipPath + " > Successfully unlocked!");
 
         //Set GoogleClientSecrets from clientsecret json file
+        System.out.println("Reading ClientSecrets file...");
         GoogleClientSecrets googleClientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
                 new InputStreamReader(zipFiles.get("clientsecret.json")));
+        System.out.println("Project ClientId: ," + googleClientSecrets.getInstalled().getClientId());
+        System.out.println("Project  AuthURI: ," + googleClientSecrets.getInstalled().getAuthUri());
+        System.out.println("Project TokenURI: ," + googleClientSecrets.getInstalled().getTokenUri());
 
-        /**--------------Scopes--------------*/
-        //Set admin scopes
-        final String[] adminScopes = new String[]{
-                "https://www.googleapis.com/auth/admin.reports.audit.readonly," +
-                "https://www.googleapis.com/auth/admin.reports.usage.readonly," +
-                "https://www.googleapis.com/auth/admin.directory.user," +
-                "https://www.googleapis.com/auth/admin.directory.group," +
-                "https://www.googleapis.com/auth/admin.directory.group.member," +
-                "https://www.googleapis.com/auth/admin.directory.customer," +
-                "https://www.googleapis.com/auth/admin.directory.domain," +
-                "https://www.googleapis.com/auth/apps.groups.settings"
-        };
-
-        //Set user scopes
-        final String[] userScopes = new String[]{
-                "https://sites.google.com/feeds/," +
-                "https://www.googleapis.com/auth/drive," +
-                "https://www.googleapis.com/auth/drive," +
-                "https://www.googleapis.com/auth/drive," +
-                "https://www.googleapis.com/auth/calendar," +
-                "https://mail.google.com/," +
-                "https://www.googleapis.com/auth/gmail.settings.basic," +
-                "https://www.googleapis.com/auth/contacts," +
-                "https://www.googleapis.com/auth/userinfo.profile," +
-                "https://www.googleapis.com/auth/userinfo.email"
-        };
-
-        ArrayList<String> allScopes = new ArrayList<>();
-        allScopes.addAll(Arrays.asList(adminScopes));
-        allScopes.addAll(Arrays.asList(userScopes));
-        System.out.println("Scopes Used: ");
-        allScopes.forEach(scope -> System.out.println("\t" + scope));
-        final ImmutableSet<String> SCOPES_SET = ImmutableSet.copyOf(allScopes);
 
         /**--------------Authentication with Google AuthFlow--------------*/
+        System.out.println("Google Authentication Flow started....");
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
                 .Builder(HTTP_TRANSPORT, JSON_FACTORY, googleClientSecrets, SCOPES_SET)
                 .setAccessType("offline")
                 .build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize(adminEmail);
-
+        System.out.println("Google Authentication Flow ended....");
         /**--------------Store tokens and settings in json file--------------*/
+        System.out.println("Writing configuration data to ," + configurationName + "_configuration.json");
         Map<String, String> configurationJsonTemplate = new HashMap<>();
         configurationJsonTemplate.put("ACCESS_TOKEN", credential.getAccessToken());
         configurationJsonTemplate.put("REFRESH_TOKEN", credential.getRefreshToken());
         configurationJsonTemplate.put("DOMAIN", domain);
         configurationJsonTemplate.put("ADMIN_EMAIL", adminEmail);
-        configurationJsonTemplate.put("SERVICE_ACCOUNT_EMAIL", "");//TODO: Client_Email from serviceAccountKey or not???
+        //configurationJsonTemplate.put("SERVICE_ACCOUNT_EMAIL", "");//TODO: Client_Email from serviceAccountKey or not???
         configurationJsonTemplate.put("CREDENTIALS_FILE_PATH", zipPath);
         configurationJsonTemplate.put("CREDENTIALS_PASSWORD", encodeString(credentialsPassword));
-        FileWriter writer = new FileWriter(new java.io.File(configurationName).getAbsolutePath());
+        FileWriter writer = new FileWriter(new java.io.File(configurationName + "_configuration.json").getAbsolutePath());
         writer.write(new GsonBuilder().setPrettyPrinting().create().toJson(configurationJsonTemplate));
         writer.close();
+        System.out.println("Configuration file ," + configurationName + "_configuration.json created successfully...");
+        System.out.println("************  Google Token Generator End ************");
     }
 
     private static String encodeString(String trueString) {
